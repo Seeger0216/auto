3869231d0564c22f60ebd608ab64169ec2c30d38
Seeger0216 <65768435+Seeger0216@users.noreply.github.com>
2021-05-03 20:34:31 +0800

    Update autoaz_stu.py

--- autoaz_stu.py
+++ autoaz_stu.py
@@ -88,14 +88,14 @@ with open("./cloud-init.txt", "w") as f:
 print("正在 " + str(location1) + " 区域创建 " + str(size1_name)+" 实例")
 get_default_cli().invoke(
           ['vm', 'create', '--resource-group', res_name, '--name',
-           f'{location1}-{size1_abbreviation}-{count}', '--image', 'UbuntuLTS',
+           f'{location1}-{size1_abbreviation}', '--image', 'UbuntuLTS',
            '--size', f'{size1_name}', '--location', f'{location1}', '--admin-username',
            'azureuser', '--admin-password', '6uPF5Cofvyjcew9', '--custom-data',
            'cloud-init.txt', "--no-wait"])
 print("正在 " + str(location2) + " 区域创建 " + str(size2_name)+" 实例")
 get_default_cli().invoke(
            ['vm', 'create', '--resource-group', res_name, '--name',
-            f'{location2}-{size2_abbreviation}-{count}', '--image', 'UbuntuLTS',
+            f'{location2}-{size2_abbreviation}', '--image', 'UbuntuLTS',
             '--size', f'{size2_name}', '--location', f'{location1}', '--admin-username',
             'azureuser', '--admin-password', '6uPF5Cofvyjcew9', '--custom-data',
             'cloud-init.txt', "--no-wait"])
