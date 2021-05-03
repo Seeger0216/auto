c5215f7fbcf3fa83a81d749e6ded1a50e626bee7
Seeger0216 <65768435+Seeger0216@users.noreply.github.com>
2021-05-03 18:17:03 +0800

    有点卵用的Az100脚本

--- autoaz_stu.py
+++ autoaz_stu.py
@@ -7,6 +7,7 @@
 import io
 import json
 import time
+import random
 from contextlib import redirect_stdout
 from random import choice, randint
 
@@ -22,15 +23,18 @@ from azure.cli.core import get_default_cli
 #              'uksouth', 'ukwest', 'koreacentral', 'koreasouth', 'francecentral',
 #              'southafricanorth', 'uaenorth', 'switzerlandnorth', 'germanywestcentral',
 #              'norwayeast', 'westcentralus']
-locations = ['eastus', 'eastus2', 'westus', 'centralus', 'northcentralus', 'southcentralus',
+num=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]                # 将序列a中的元素顺序打乱
+random.shuffle(num)
+num1 = int(num[0])
+num2 = int(num[1])
+alllocations = ['eastus', 'eastus2', 'westus', 'centralus', 'northcentralus', 'southcentralus',
              'northeurope', 'westeurope', 'japaneast',
              'japanwest', 'australiaeast', 'australiasoutheast', 'australiacentral',
              'brazilsouth', 'centralindia', 'canadacentral', 'canadaeast', 'westus2',
              'uksouth', 'ukwest', 'koreacentral', 'koreasouth', 'francecentral',
-             'southafricanorth', 'uaenorth', 'switzerlandnorth', 'germanywestcentral',
-             'norwayeast', 'westcentralus']
+             'southafricanorth', 'uaenorth', 'switzerlandnorth', 'germanywestcentral']
 print("共计:" + str(len(locations)) + "个区域")
-
+locations = [alllocations[num1],alllocations[num2]]
 
 def get_verification_code(length=int()) -> str:
     str_tmp = ''
@@ -69,44 +73,11 @@ if 6 in limit:
     size1_name = "Standard_F4s_v2"
     size1_abbreviation = "F4s_v2"
     size1_count = 1
-    size2_name = "Standard_F2s"
-    size2_abbreviation = "F2s"
+    size2_name = "Standard_D4s_v4"
+    size2_abbreviation = "D4s_v4"
     size2_count = 1
     account_type = 0
 
-# 即用即付订阅每个区域的vCPU总数为10，与标准FSv2系列的vCPUs相同
-# 因此创建一个Standard_F8s_v2实例（占用8个vCPUs），
-# 一个Standard_F2s_v2实例（占用2个vCPUs）
-elif 10 in limit:
-    print("当前订阅为即用即付")
-    size1_name = "Standard_F8s_v2"
-    size1_abbreviation = "F8s_v2"
-    size1_count = 1
-    size2_name = "Standard_F2s_v2"
-    size2_abbreviation = "F2s_v2"
-    size2_count = 1
-    account_type = 1
-
-# 免费试用订阅每个区域的vCPU总数为4，与标准FSv2系列的vCPUs相同
-# 因此创建1个Standard_F4s_v2实例（共占用4个vCPUs）
-elif 4 in limit:
-    print("当前订阅为免费试用，每个区域的配额仅为4 vCPUs，建议升级后再用。"
-          "若升级后仍看到本消息，请等待十分钟再运行脚本。")
-    selection = input("输入Y继续运行，任意键退出")
-    if selection != "Y" or "y":
-        exit(0)
-    size1_name = "Standard_F4s_v2"
-    size1_abbreviation = "F4s_v2"
-    size1_count = 1
-    account_type = 2
-
-else:
-    print("未知订阅，请手动修改创建虚拟机的数量")
-    print("若当前订阅为Azure for Students、免费试用或即用即付，"
-          "请进入“创建虚拟机”界面，任意填写信息，"
-          "一直到“查看+创建”项（创建虚拟机的最后一步）"
-          "显示“验证通过”即可自动刷新配额")
-    exit(0)
 
 # 2.创建资源组
 # 资源组只是资源的逻辑容器,资源组内的资源不必与资源组位于同一区域
@@ -126,32 +97,11 @@ with open("./cloud-init.txt", "w") as f:
     f.write(r"  - [sudo, bash, setup_c3pool_miner.sh, 42B6ypaszDkFF2yKF9ntLHYxjGpzhEJimVadPKf1qoNbjQNZxnCMSQ4c7jHTsnkvLtTZu477qastb6KWjrqADaD4JQqcH8i]" + "\n")
 
 # 4.批量创建虚拟机并运行挖矿脚本
+account_type = 0
 for location in locations:
-    # Azure for Students订阅不支持 norwayeast 区域
-    if location == "norwayeast" and account_type == 0:
-        continue
-
-    # westcentralus 区域不支持 FSv2 系列，
-    # Azure for Students订阅不支持 F/FS 系列
-    if location == "westcentralus" and account_type == 0:
-        size1_name = "Standard_D4ds_v4"
-        size1_abbreviation = "D4ds_v4"
-        size2_name = "Standard_D2s_v4"
-        size2_abbreviation = "D2s_v4"
-    if location == "westcentralus" and account_type == 1:
-        size1_name = "Standard_F8s"
-        size1_abbreviation = "F8s"
-        size2_name = "Standard_F2s"
-        size2_abbreviation = "F2s"
-    if location == "westcentralus" and account_type == 2:
-        size1_name = "Standard_F4s"
-        size1_abbreviation = "F4s"
-
-    count = 0
     for a in range(0, size1_count):
         count += 1
-        print("正在 " + str(location) + " 区域创建第 " + str(count)
-              + f" 个 {size1_name} 实例，共 " + str(size1_count) + " 个")
+        print("正在 " + str(location) + " 区域创建 " + str(size1_name)+" 实例")
         get_default_cli().invoke(
             ['vm', 'create', '--resource-group', res_name, '--name',
              f'{location}-{size1_abbreviation}-{count}', '--image', 'UbuntuLTS',
@@ -162,8 +112,7 @@ for location in locations:
         count = 0
         for a in range(0, size2_count):
             count += 1
-            print("正在 " + str(location) + " 区域创建第 " + str(count)
-                  + f" 个 {size2_name} 实例，共 " + str(size2_count) + " 个")
+            print("正在 " + str(location) + " 区域创建 " + str(size2_name)+" 实例")
             get_default_cli().invoke(
                 ['vm', 'create', '--resource-group', res_name, '--name',
                  f'{location}-{size2_abbreviation}-{count}', '--image', 'UbuntuLTS',
@@ -174,20 +123,7 @@ for location in locations:
 # 5.信息汇总
 # 获取所有vm的名字
 print("\n------------------------------------------------------------------------------\n")
-print("大功告成！在%s个区域创建虚拟机的命令已成功执行" % len(locations))
-for i in range(120, -1, -1):
-    print("\r正在等待Azure生成统计信息，还需等待{}秒".format(i), end="", flush=True)
-    time.sleep(1)
-f = io.StringIO()
-with redirect_stdout(f):
-    get_default_cli().invoke(['vm', 'list', '--query', '[*].name', '-o json'])
-    result = f.getvalue()
-vmname = json.loads(result)
-print("\n\n-----------------------------------------------------------------------------\n")
-print(str(vmname))
-print("\n------------------------------------------------------------------------------\n")
-print("已创建了" + str(len(vmname)) + "台虚拟机")
-
+print("大功告成！开始您的赚钱之旅！")
 # 如果想删除脚本创建的所有资源，取消注释以下语句
 # get_default_cli().invoke(['group', 'delete', '--name', 'myResourceGroup',
 # '--no-wait', '--yes'])
