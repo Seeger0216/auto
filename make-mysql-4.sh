1cb5f5e0de2c4d4186884a9712f0ec1663a83711
Seeger0216 <65768435+Seeger0216@users.noreply.github.com>
2022-11-18 21:56:40 +0800

    Update and rename make-mysql.sh to make-mysql-4.sh

--- make-mysql-4.sh
+++ make-mysql-4.sh
@@ -25,7 +25,7 @@ else
 	tar -xzv -f mysql-boost-5.7.18.tar.gz 
 	cd mysql-5.7.18/
 	cmake -DCMAKE_INSTALL_PREFIX=/usr/local/mysql -DMYSQL_DATADIR=/usr/local/mysql/data -DWITH_BOOST=./boost/boost_1_59_0 -DSYSCONFDIR=/etc -DWITH_INNOBASE_STORAGE_ENGINE=1 -DWITH_PARTITION_STORAGE_ENGINE=1 -DWITH_FEDERATED_STORAGE_ENGINE=1 -DWITH_BLACKHOLE_STORAGE_ENGINE=1 -DWITH_MYISAM_STORAGE_ENGINE=1 -DWITH_MEMORY_STORAGE_ENGINE=1 -DENABLED_LOCAL_INFILE=1 -DWITH_READLINE=1 -DMYSQL_TCP_PORT=3306 -DEXTRA_CHARSETS=all -DDEFAULT_CHARSET=utf8 -DDEFAULT_COLLATION=utf8_general_ci
-	make
+	make -j 4
 	if [ $? != 0 ];then
 		echo "----------------------------Install error!!!----------------------------"
 		exit
